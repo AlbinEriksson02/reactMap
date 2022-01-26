fb4ee27006c7448ab7a48c528c918ff7bab68644
PontusForsmark <pontus.forsmark@elev.ga.ntig.se>
2022-01-26 15:43:03 +0100

    Refactored popup infobox to use map

--- src/sampleMap.jsx
+++ src/sampleMap.jsx
@@ -1,19 +1,24 @@
 import React from 'react';
 import { MapContainer, TileLayer} from 'react-leaflet';
-import { InfoBox } from './popups/InfoBox'
+import { InfoBox } from './components/InfoBox'
 
 export const SampleMap = () => {
-  const positions = [[59.840198377930875, 17.650249600410465],[59.841025697294626, 17.650178740444606]];
+  const data = [{position: [59.840198377930875, 17.650249600410465], name: 'Te4'}, {position: [59.841025697294626, 17.650178740444606], name: 'Rullan'},];
+  console.log(data);
   
   return (
     <div style={{ height: '400px', width: '400px', background: 'gray' }}>
-      <MapContainer center={positions[0]} zoom={15} scrollWheelZoom={true}  >
+      <MapContainer center={data[0].position} zoom={15} scrollWheelZoom={true}  >
         <TileLayer
           attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
           url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
         />
-            <InfoBox position= {positions[0]} name='Te4'/>
-            <InfoBox position= {positions[1]} name='Rullan'/>
+        {
+          data.map((marker) => (
+            <InfoBox position={marker.position} name={marker.name} key={marker.name}/>
+          ))
+        }
+        
       </MapContainer>
     </div>
   );
