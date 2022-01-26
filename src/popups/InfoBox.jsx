dd4c74934a76f1b2de6b6c6a051d225bbad10b3c
PontusForsmark <pontus.forsmark@elev.ga.ntig.se>
2022-01-26 15:15:02 +0100

    Refactored popups

--- src/popups/InfoBox.jsx
+++ src/popups/InfoBox.jsx
@@ -1,11 +1,11 @@
 import { Popup, Marker } from 'react-leaflet';
 import React from 'react';
 
-export const Te4 = (props) => {
+export const InfoBox = (props) => {
     return (
         <Marker position={props.position}>
             <Popup>
-                <h1>Te4</h1>
+                <h1>{props.name}</h1>
             </Popup>
         </Marker>
     );
--- src/sampleMap.jsx
+++ src/sampleMap.jsx
@@ -1,18 +1,19 @@
 import React from 'react';
 import { MapContainer, TileLayer} from 'react-leaflet';
-import { Te4 } from './popups/te4'
+import { InfoBox } from './popups/InfoBox'
 
 export const SampleMap = () => {
-  const position_te4 = [59.840198377930875, 17.650249600410465];
-  console.log(position_te4);
+  const positions = [[59.840198377930875, 17.650249600410465],[59.841025697294626, 17.650178740444606]];
+  
   return (
     <div style={{ height: '400px', width: '400px', background: 'gray' }}>
-      <MapContainer center={position_te4} zoom={15} scrollWheelZoom={true}  >
+      <MapContainer center={positions[0]} zoom={15} scrollWheelZoom={true}  >
         <TileLayer
           attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
           url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
         />
-            <Te4 position= {position_te4}/>
+            <InfoBox position= {positions[0]} name='Te4'/>
+            <InfoBox position= {positions[1]} name='Rullan'/>
       </MapContainer>
     </div>
   );
